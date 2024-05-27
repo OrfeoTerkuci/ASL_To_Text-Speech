730f4cc83d951948fcb603d895ca8231eb36854c
Orfeo Terkuci <terkuciorfeo@gmail.com>
2024-05-27 15:14:57 +0200

    chore: Update image_processing.py to use type annotations for mp_hands, mp_draw, and mp_styles

--- source/image_rendering/image_processing.py
+++ source/image_rendering/image_processing.py
@@ -58,14 +58,14 @@ class MpSettings:  # pylint: disable=R0903
     """
 
     def __init__(self) -> None:
-        self.mp_hands = mp.solutions.hands
+        self.mp_hands = mp.solutions.hands  # type: ignore
         self.hands = self.mp_hands.Hands(
             static_image_mode=True,
             max_num_hands=2,
             min_detection_confidence=0.5,
         )
-        self.mp_draw = mp.solutions.drawing_utils
-        self.mp_styles = mp.solutions.drawing_styles
+        self.mp_draw = mp.solutions.drawing_utils  # type: ignore
+        self.mp_styles = mp.solutions.drawing_styles  # type: ignore
 
     def __del__(self) -> None:
         self.hands.close()
