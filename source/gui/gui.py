3bbcc2dcd0c7bc2d878282cce1d0158dbe65dc4e
anass hamzaoui <Anass.hamzaoui@student.uantwerpen.be>
2024-05-30 14:01:03 +0200

    gui updated and finished

--- source/gui/gui.py
+++ source/gui/gui.py
@@ -1,6 +1,18 @@
-import tkinter
+import tkinter as tk 
 import tkinter.messagebox
 import customtkinter as ctk
+
+import sys
+import os
+
+# Get the path of the 'src' directory
+src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add the 'src' directory to the Python path
+sys.path.append(src_dir)
+
+from image_rendering.image_recognition import WebcamHandtracking
+from models.cnn.cnn import CNN
 
 ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -8,7 +20,7 @@ ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-
+        
         # configure window
         self.title("ASL Recognition System - GUI")
         self.geometry(f"{1100}x{580}")
@@ -25,13 +37,15 @@ class App(ctk.CTk):
         self.logo_label = ctk.CTkLabel(self.sidebar_frame, text="Models", font=ctk.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
         
+        self.current_model = None
+        
          # Change to CNN model
-        self.CNN_model = ctk.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.CNN_model = ctk.CTkButton(self.sidebar_frame, command=lambda: self.sidebar_button_event("CNN Model"))
         self.CNN_model.configure(text="CNN Model", corner_radius=4)
         self.CNN_model.grid(row=1, column=0, padx=20, pady=10)
         
         # Change to VIT model
-        self.VIT_model = ctk.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
+        self.VIT_model = ctk.CTkButton(self.sidebar_frame, command=lambda: self.sidebar_button_event("VIT Model"))
         self.VIT_model.configure(text="VIT Model", corner_radius=4)
         self.VIT_model.grid(row=2, column=0, padx=20, pady=10)
         
@@ -46,11 +60,10 @@ class App(ctk.CTk):
         self.scaling_optionemenu = ctk.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
-
-        # create textbox
-        self.textbox = ctk.CTkTextbox(self, width=250)
-        self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
-
+        
+        self.webcam_button = ctk.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), command=lambda: self.start_tracking())
+        self.webcam_button.configure(text="Start Tracking", corner_radius=4)
+        self.webcam_button.grid(row=0, rowspan=4, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
     def open_input_dialog_event(self):
         dialog = ctk.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
@@ -63,20 +76,23 @@ class App(ctk.CTk):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         ctk.set_widget_scaling(new_scaling_float)
 
-    def sidebar_button_event(self):
-        if self.CNN_model.cget("text") == "CNN Model":
+    def sidebar_button_event(self, Button_name: str):
+        if Button_name == "CNN Model":
             self.CNN_model.configure(text="✔ Loaded CNN Model")
             self.VIT_model.configure(text="VIT Model")
             # TODO: Implement Logic for changing to CNN model
-            print("CNN_button click")
-        elif self.VIT_model.cget("text") == "VIT Model":
-            self.VIT_model.configure(text="✔ Loaded VIT Model")
+            self.current_model = CNN("","","", False)
+            self.current_model.load("./models/best_models/cnn_1e-07_32_1000_0.004_2_6.pth")
+        elif Button_name == "VIT Model":
+            # self.VIT_model.configure(text="✔ Loaded VIT Model")
             self.CNN_model.configure(text="CNN Model")
             # TODO: Implement Logic for changing to VIT model
-            print("VIT_button click")
-        print("sidebar_button click")
+            self.current_model = None
+            tkinter.messagebox.showinfo("VIT Model", "VIT Model coming soon")
+        
+    def start_tracking(self):
+        webcam = WebcamHandtracking()
+        if self.current_model is not None:
+            webcam.load_model(self.current_model)
+        webcam.multi_threading_hand_tracking()
 
-
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
